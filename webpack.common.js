45bd5d0e27bc74cb56262ee2a6d8fbd7bc7b30a8
Vyacheslav Zababurin <vvzababurin@gmail.com>
2022-04-19 10:44:53 +0300

    Last version

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [ 'babel-polyfill', './src/main.js' ],
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'production' }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    chunkFilename: "[id].bundle.js"
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/,
+	      use: { 
+		      loader: 'babel-loader',  
+		      options: {
+			      presets: ['@babel/preset-env']
+        	} 
+	      },
+      },
+      { 
+        test: /\.wgsl/,
+        type: 'asset/source' 
+      }
+    ]
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  mode: 'production'
+};
