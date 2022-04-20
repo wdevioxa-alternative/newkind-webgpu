4361c515dabbf161506b0bc87c0412f69913bbbc
Vyacheslav Zababurin <vvzababurin@gmail.com>
2022-04-20 20:23:40 +0300

    Pretty

--- src/box.js
+++ src/box.js
@@ -28,13 +28,10 @@ export class GBox extends GObject
         let positions = new Float32Array([
             instance.calcX(1+offsetx), instance.calcY(1+objectheight+offsety), 0.0,
             instance.calcX(1+objectwidth+offsetx), instance.calcY(1+objectheight+offsety), 0.0,
-    
             instance.calcX(1+objectwidth+offsetx), instance.calcY(1+objectheight+offsety), 0.0,
             instance.calcX(1+objectwidth+offsetx), instance.calcY(1+offsety), 0.0,
-    
             instance.calcX(1+objectwidth+offsetx), instance.calcY(1+offsety), 0.0,
             instance.calcX(1+offsetx), instance.calcY(1+offsety), 0.0,
-    
             instance.calcX(1+offsetx), instance.calcY(0+offsety), 0.0,
             instance.calcX(1+offsetx), instance.calcY(1+objectheight+offsety), 0.0
         ]);
