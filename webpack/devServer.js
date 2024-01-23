21d698eba32816b751b39275be3612a2b96da7a1
vvzababurin <vvzababurin@hotmail.com>
2024-01-23 14:20:44 +0300

    remove all

--- webpack/devServer.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  devServer: {
-    port: 7355
-  },
-  watchOptions: {
-    ignored: /node_modules/,
-  },
-};
--- webpack/webpack.dev.mjs
+++
@@ -1,7 +0,0 @@
-import { merge } from 'webpack-merge';
-import common from './webpack.common.mjs';
-import server from './devServer.mjs';
-
-export default merge(common, server, {
-  mode: 'development'
-});
