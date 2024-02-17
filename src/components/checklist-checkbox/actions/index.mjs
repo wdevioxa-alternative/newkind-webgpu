9f8e5d107a57d0d61660e7cd537069daf9803a9b
Zababurin <zababurinsv@vk.com>
2024-02-17 22:41:09 +0300

    update

--- src/components/checklist-checkbox/actions/index.mjs
+++ src/components/checklist-checkbox/actions/index.mjs
@@ -6,8 +6,15 @@ export const actions = (self) => {
         resolve({
             click: (event) => {
                 event.stopPropagation()
-                self.setAttribute('checked', true)
-                console.log('---------------------- CLICK CHECKBOX --------------------------------', self)
+                // self.setAttribute('checked', true)
+                // const customCheckbox = self.shadowRoot.querySelector('.custom-checkbox')
+                // const input = self.shadowRoot.querySelector('input')
+                //
+                // input.setAttribute("checked", true);
+                // customCheckbox.setAttribute("checked", true);
+                // self.setAttribute("checked", true);
+
+                // console.log('---------------------- CLICK CHECKBOX --------------------------------', customCheckbox)
             }
         })
     })
