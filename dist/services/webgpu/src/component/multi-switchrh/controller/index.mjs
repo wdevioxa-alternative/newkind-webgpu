ae1899e771e2087de21109ff592cdde06548de69
Zababurin <zababurinsv@vk.com>
2024-02-22 01:40:40 +0300

    update

--- dist/services/webgpu/src/component/multi-switchrh/controller/index.mjs
+++
@@ -1,9 +0,0 @@
-import addEventListener from './addEventListener/index.mjs'
-export const controller = (self, actions) => {
-    return new Promise(async (resolve, reject) => {
-
-        resolve({
-            addEventListener: await addEventListener(self, actions)
-        })
-    })
-}
