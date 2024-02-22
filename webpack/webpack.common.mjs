90ec51fccb80f153b22b55919058bd0109ebdcb0
Vyacheslav Zababurin <v_vzababurin@vk.com>
2024-02-22 04:42:33 +0300

    update

--- webpack/webpack.common.mjs
+++ webpack/webpack.common.mjs
@@ -14,70 +14,69 @@ export default {
         topLevelAwait: true
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
-        }),
-        new HtmlWebpackPlugin({ 
-            title: 'WebGPU Test Page',
-            filename: 'index.html',
-            template: './src/index.template',
-            font: 'fonts/segoeuil.ttf'
-        }),
-        new RemoveWebpackPlugin({
-          before: {
-            log: false,
-            include: [ 'dist' ]
-          }
-        }),
-        new CopyWebpackPlugin({ 
-	        patterns: [
-            { from: path.resolve(__dirname, 'src', 'font'), to: path.resolve(__dirname, 'dist', 'font') },
-            { from: path.resolve(__dirname, 'src', 'js'), to: path.resolve(__dirname, 'dist', 'js') },
-            { from: path.resolve(__dirname, 'src', 'assets'), to: path.resolve(__dirname, 'dist', 'assets') },
-            { from: path.resolve(__dirname, 'src', 'config'), to: path.resolve(__dirname, 'dist', 'config') },
-            { from: path.resolve(__dirname, 'src', 'sounds'), to: path.resolve(__dirname, 'dist', 'sounds') }
-          ]
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-  ],
-  output: {
-	path: path.resolve(__dirname, 'dist'),
-	filename: "[name].bundle.js",
-	chunkFilename: "[id].bundle.js",
-	assetModuleFilename: "[path][name].[ext]",
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
+      }),
+      new HtmlWebpackPlugin({ 
+        title: 'WebGPU Test Page',
+        filename: 'index.html',
+        template: './src/index.template',
+        font: 'fonts/segoeuil.ttf'
+      }),
+      new RemoveWebpackPlugin({
+        before: {
+          log: false,
+          include: [ 'dist' ]
+        }
+      }),
+      new CopyWebpackPlugin({ 
+	      patterns: [
+          { from: path.resolve(__dirname, 'src', 'font'), to: path.resolve(__dirname, 'dist', 'font') },
+          { from: path.resolve(__dirname, 'src', 'js'), to: path.resolve(__dirname, 'dist', 'js') },
+          { from: path.resolve(__dirname, 'src', 'assets'), to: path.resolve(__dirname, 'dist', 'assets') },
+          { from: path.resolve(__dirname, 'src', 'config'), to: path.resolve(__dirname, 'dist', 'config') },
+          { from: path.resolve(__dirname, 'src', 'sounds'), to: path.resolve(__dirname, 'dist', 'sounds') }
+        ]
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+	    chunkFilename: "[id].bundle.js",
+	    assetModuleFilename: "[path][name].[ext]",
     // publicPath: path.resolve(__dirname, 'dist/this'),
-  },
-  module: {
-	rules: [{
-            test: /\.html$/,
-            loader: "raw-loader"
+    },
+    module: {
+	    rules: [{
+        test: /\.html$/,
+        loader: "raw-loader"
+      },
+      { 
+		    test: /\.(jsx|mjs)$/, 
+        exclude: /\.(node_modules|js)$/,
+        use: { 
+          loader: 'babel-loader',  
+          options: {
+            presets: ['@babel/preset-env']
+          } 
         },
-	{ 
-		test: /\.(jsx|mjs)$/, 
-		exclude: /\.(node_modules|js)$/,
-		use: { 
-			loader: 'babel-loader',  
-			options: {
-				presets: ['@babel/preset-env']
-			} 
-		},
-	},
-	{ 
-		test: /\.wgsl/,
-		type: 'asset/source' 
-	},
-	{ 
-		test: /\.(eot|svg|ttf|woff|woff2)$/,
-		type: 'asset/resource',
-		generator: {
-			filename: '[path][name].[ext]'
-		}
-	}]
-  },
-  performance: {
-	hints: false,
-	maxEntrypointSize: 512000,
-	maxAssetSize: 512000
-  }
-
+      },
+      { 
+        test: /\.wgsl/,
+        type: 'asset/source' 
+      },
+      { 
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: '[path][name].[ext]'
+        }
+      }]
+    },
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    }
 };
